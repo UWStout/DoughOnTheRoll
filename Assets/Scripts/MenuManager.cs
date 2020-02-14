f815c734767ca02db2d9848e6bca74f59d4f069f
Schultz <schultzj7737@uwstout.edu>
2020-02-14 14:07:13 -0600

    Main menu switching

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject[] menuScreens = new GameObject[4];
+
+    public void switchMenu(int index)
+    {
+        for(int i = 0; i < menuScreens.Length; i++)
+        {
+            if(i != index)
+            {
+                menuScreens[i].SetActive(false);
+            }
+            else if (i == index)
+            {
+                menuScreens[i].SetActive(true);
+            }
+        }
+    }
+}
